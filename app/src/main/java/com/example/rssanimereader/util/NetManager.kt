7f31f8f13a69691f5c9c4f74acd9497a75d6acc8
igamgam97 <igamgam97@gmail.com>
2019-04-21 16:27:04 +0300

    add NetManager wrapper for check internet connection

---
+++ app/src/main/java/com/example/rssanimereader/util/NetManager.kt
@@ -0,0 +1,15 @@
+package com.example.rssanimereader.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+class NetManager(private var applicationContext: Context) {
+    private var status: Boolean? = false
+
+    val isConnectedToInternet: Boolean?
+        get() {
+            val conManager = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val ni = conManager.activeNetworkInfo
+            return ni != null && ni.isConnected
+        }
+}
