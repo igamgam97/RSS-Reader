3c94386186ed2347097625842cbcb7ad4e2cb923
igamgam97 <igamgam97@gmail.com>
2019-04-21 19:36:06 +0300

    add FeedRecyclerViewAdapter for RecyclerView add DataBinding logic in MainActivity add RecyclerView and data in activity_main.xml

---
+++ app/src/main/java/com/example/rssanimereader/FeedRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.rssanimereader
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rssanimereader.data.FeedItem
+import com.example.rssanimereader.databinding.RvItemFeedBinding
+
+class FeedRecyclerViewAdapter(
+    private var items: ArrayList<FeedItem>,
+    private var listener: OnItemClickListener
+) : RecyclerView.Adapter<FeedRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = RvItemFeedBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position], listener)
+
+    override fun getItemCount(): Int = items.size
+
+    fun replaceData(items: ArrayList<FeedItem>) {
+        this.items = items
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    class ViewHolder(private var binding: RvItemFeedBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(feed: FeedItem, listener: OnItemClickListener?) {
+            binding.feed = feed
+            if (listener != null) binding.root.setOnClickListener { listener.onItemClick(layoutPosition) }
+
+            binding.executePendingBindings()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rssanimereader/view/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.rssanimereader.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.rssanimereader.FeedRecyclerViewAdapter
+import com.example.rssanimereader.R
+import com.example.rssanimereader.data.FeedItem
+import com.example.rssanimereader.databinding.ActivityMainBinding
+import com.example.rssanimereader.viewmodel.MainViewModel
+
+class MainActivity : AppCompatActivity(), FeedRecyclerViewAdapter.OnItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private val feedRecyclerViewAdapter = FeedRecyclerViewAdapter(arrayListOf(), this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.viewModel = viewModel
+        binding.executePendingBindings()
+
+        binding.feedRv.layoutManager = LinearLayoutManager(this)
+        binding.feedRv.adapter = feedRecyclerViewAdapter
+        viewModel.feeds.observe(this, Observer<ArrayList<FeedItem>> {
+            it?.let {
+                feedRecyclerViewAdapter.replaceData(it)
+                Log.d("baging", it.size.toString())
+            }
+        })
+    }
+
+    override fun onItemClick(position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
