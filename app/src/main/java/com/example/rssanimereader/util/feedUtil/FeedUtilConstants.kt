4dfbf3753d2eca9d8d9589e0220e94a76837a3f6
igamgam97 <igamgam97@gmail.com>
2019-04-24 23:46:57 +0300

    create constant class

---
+++ app/src/main/java/com/example/rssanimereader/util/feedUtil/FeedUtilConstants.kt
@@ -0,0 +1,9 @@
+package com.example.rssanimereader.util.feedUtil
+
+object FeedUtilConstants {
+    const val URL = "urlpath"
+    const val BROADCAST_STATE_DATA_ACTION = "Status"
+    const val READ_TIMEOUT_VALUE = 15000
+    const val CONNECT_TIMEOUT_VALUE = 10000
+    const val STATUS_DATA = "StatusData"
+}
